97f4aaf1b24b39e6cbf5bb8406dbb8207617bdf0
dtpco <donnalyn.topacio@cit.edu>
2024-02-24 14:12:24 +0800

    Initial101 commit

--- LogInSystem_YTTutorial/urls.py
+++ LogInSystem_YTTutorial/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('base/', views.base, name='base'),
+    path('home/', views.base, name='home'),
 ]
--- LogInSystem_YTTutorial/views.py
+++ LogInSystem_YTTutorial/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return render(request, "LIS/index.html")
+    return render(request, "LIS/../templates/index.html")
 
 
 def signup(request):
@@ -17,10 +20,29 @@ def signup(request):
         password = request.POST['password']
         confirm = request.POST['confirm']
 
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+        return redirect("signin")
     return render(request, "LIS/signup.html")
 
 
 def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "LIS/home.html")
+        else:
+            messages.error(request, "Invalid")
+            return redirect("index")
+
     return render(request, "LIS/signin.html")
 
 
@@ -28,3 +50,5 @@ def base(request):
     return render(request, "LIS/base.html")
 
 
+def home(request):
+    return render(request, "LIS/home.html")
